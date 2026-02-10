a7134b62ede65a9ff777ea2ae0c575280e8cebb6
Joshitha-Bandi <joshitha.bandi1406@gmail.com>
2026-02-10 18:52:43 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first name: ").capitalize()
+last_name = input("Enter your last name: ").capitalize()
+
+full_name = first_name + " " + last_name
+
+print(f"Hello, {full_name}! Welcome to Python programming.")
